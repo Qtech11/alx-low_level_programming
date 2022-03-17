d6712280be63dd0de8763b7ce3d2813ae530c160
Qtech11 <olanrewajukudus@gmail.com>
2022-03-17 20:42:54 +0100

    first

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,23 +1,13 @@
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H
 
-void print_alphabet(void);
+#include <stdio.h>
 
-void print_alphabet_x10(void);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 
-int _islower(int c);
+void print_remaining_days(int month, int day, int year);
 
-int _isalpha(int c);
+int convert_day(int month, int day);
 
-int print_sign(int n);
-
-int _abs(int n);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int, int);
-
-void print_to_98(int n);
+#endif /* MAIN_H */
